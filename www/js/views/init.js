b3c925ab16350f62bc95ce1bee848e8869e06b73
Judy <jsyrkinn@macalester.edu>
2015-02-25 13:24:23 -0600

    Beginning of Organization
    Before 2/25

---
+++ www/js/views/init.js
@@ -0,0 +1,17 @@
+//init views
+
+var stage = null;
+
+document.addEventListener('deviceready', function() {
+
+  // create an new instance of a pixi stage
+  var interactive = true
+  stage = new PIXI.Stage(0x66FF99, interactive);
+
+  // create a renderer instance.
+  var renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight, {resolution:2});
+
+  // add the renderer view element to the DOM
+  document.body.appendChild(renderer.view);
+
+  requestAnimFrame( animate );
---
+++ www/js/views/playerHand.js
@@ -0,0 +1,27 @@
+
+
+function handPosRecalc(hand) {
+  //posInitial = window.innerWidth*0.20;
+  //posOffset = window.innerWidth*0.15;
+
+  rotInitial = -0.25;
+  rotOffset = 0.08;
+
+  cardWidth = hand[0].sprite.width;
+  cardHeight = hand[0].sprite.height;
+
+  for(c = 0; c < hand.length; c++) {
+
+    //remove and re-add sprite to ensure it's rendered last
+    stage.removeChild(hand[c].sprite);
+    stage.addChild(hand[c].sprite);
+
+    hand[c].sprite.anchor.x = 0;
+    hand[c].sprite.anchor.y = 2;
+
+    hand[c].sprite.position.x = cardWidth*0.75;
+    hand[c].sprite.position.y = window.innerHeight+cardHeight;
+
+    hand[c].sprite.rotation = rotInitial+(c*rotOffset);
+  }
+};
