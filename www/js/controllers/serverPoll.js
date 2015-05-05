c416593db5837e2129a69f051fb9013fdd086407
Jammf <jameymmf@gmail.com>
2015-05-05 17:21:09 -0500

    added missing semicolon

--- www/js/controllers/serverPoll.js
+++ www/js/controllers/serverPoll.js
@@ -141,6 +141,7 @@ function updateGame() {
     if (this.status >= 200 && this.status < 400) {
       var serverGameState = JSON.parse(this.response);
         gameState = serverGameState;
+        window.gameState = gameState;
         var modelGameState = new ModelGameState(gameState);
         createGameStateView(modelGameState);
     }
