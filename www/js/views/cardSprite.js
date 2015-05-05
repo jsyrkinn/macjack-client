181a23dc0b0dea8450723d83c033f2d98b83238a
igreene17 <igreene@macalester.edu>
2015-05-05 16:40:40 -0500

    Cleaned up spacing and code in cardSprite

--- www/js/views/cardSprite.js
+++ www/js/views/cardSprite.js
@@ -2,25 +2,8 @@ function CardSprite(card) {
   this.rank = card.rank;
   this.suit = card.suit;
   this.sprite = new PIXI.Sprite.fromImage("img/cards/" + this.suit.toLowerCase() + this.rank + ".png");
-
-  this.sprite.anchor.x = 0.5;
-  this.sprite.anchor.y = 0.5;
-
-  this.sprite.scale.x = 0.25;
-  this.sprite.scale.y = 0.25;
-
+  this.sprite.anchor = {x: 0.5, y: 0.5}; 
+  this.sprite.scale = {x:0.25, y: 0.25};
   this.sprite.visible = true;
-
   window.stage.addChild(this.sprite);
-
-  this.sprite.tap = function(touchData){
-    console.log("TAP!");
-    console.log(card);
-  }
-
-  //TODO: change this after ranks are changed to integers
-  this.toString = function() {
-    return this.rank + " of " + this.suit;
-  }
-
 }
