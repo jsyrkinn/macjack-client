47cd8b5377aa0b8e1c4dcb853f1da0ccdeb2bdb4
igreene17 <igreene@macalester.edu>
2015-05-05 16:19:16 -0500

    Cleaned up spacing and comments in cardPile

--- www/js/models/cardPile.js
+++ www/js/models/cardPile.js
@@ -1,4 +1,3 @@
-
 function CardPile(hand) {
   this.cards = hand.cards;
   this.bet = hand.bet;
@@ -9,36 +8,27 @@ function CardPile(hand) {
     var tot21 = 0;
     var totals = [];
     totals[0] = 0;
-
     this.cards.forEach(function(card) {
       for (var i=totals.length-1; i >= 0; i--) {
-        if (card.rank == 1) {
-          // ace
-          if (totals.indexOf(totals[i]+1) == -1) {
+        if (card.rank == 1) { //Card is an ace. 
+          if (totals.indexOf(totals[i]+1) == -1) { // The new total is not in the totals arrray. 
             totals.push(totals[i] + 1);
           }
           totals[i] += 11;
-
-        } else if (card.rank < 10) {
-          // number card
+        } else if (card.rank < 10) { // Card is a number card. 
           totals[i] += card.rank;
-
-        } else {
-          // face card
+        } else { // Card is a face card.
           totals[i] += 10;
         }
-
         if (totals[i] > 21) {
           tot21 = totals[i];
           totals.splice(i,1);
         }
       }
-    });
-
+    }
     if (totals.length == 0) {
       totals = tot21
     }
-
     return totals.sort();
   }
 }
