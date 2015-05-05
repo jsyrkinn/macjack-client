57a5e8f46febf8657e37dde31504911c90e40e82
Jammf <jameymmf@gmail.com>
2015-05-05 15:51:48 -0500

    Fixed sumTotal

--- www/js/models/cardPile.js
+++ www/js/models/cardPile.js
@@ -10,34 +10,35 @@ function CardPile(hand) {
     var totals = [];
     totals[0] = 0;
 
-    for (var i = 0; i < this.cards.length; i++ ) {
-      var card = this.cards[i];
+    this.cards.forEach(function(card) {
+      for (var i=totals.length-1; i >= 0; i--) {
+        if (card.rank == 1) {
+          // ace
+          if (totals.indexOf(totals[i]+1) == -1) {
+            totals.push(totals[i] + 1);
+          }
+          totals[i] += 11;
 
-      var totLength = totals.length;
-      for (var b=totals.length-1; b >= 0; b--) {
-
-        if (card.rank != 1 && card.rank <= 10) {
-          totals[b] += card.rank;
-
-        } else if (card.rank > 10){
-          totals[b] += 10;
+        } else if (card.rank < 10) {
+          // number card
+          totals[i] += card.rank;
 
         } else {
-          totals.push(totals[b] + 11);
-          totals[b]+=1;
+          // face card
+          totals[i] += 10;
         }
 
-        if (totals[b] > 21) {
-          tot21 = totals[b];
-          totals.splice(b,1);
+        if (totals[i] > 21) {
+          tot21 = totals[i];
+          totals.splice(i,1);
         }
       }
-    }
+    });
 
     if (totals.length == 0) {
       totals = tot21
     }
 
-    return totals;
+    return totals.sort();
   }
 }
